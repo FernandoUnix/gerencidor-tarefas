e2c08e05b9e94ec5bbbad6b9ee0faf2b3a922b79
Fernando Amaro <01fernando.ti@gmail.com>
2023-08-28 15:53:08 -0300

    Adicionado mais testes de rota da api

--- src/main/java/com/gerenciador/tarefas/controller/TesteApiController.java
+++ src/main/java/com/gerenciador/tarefas/controller/TesteApiController.java
@@ -1,6 +1,7 @@
 package com.gerenciador.tarefas.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,15 @@ public class TesteApiController {
     @GetMapping("/teste-api")
     private String teste() {
         return "Sucesso";
+    }
+    
+    @GetMapping("/teste-api-bem-vindo")
+    private String testeBemVindo(@RequestParam(name = "nome") String nome) {
+        return "Olá, seja muito bem vindo " + nome;
+    }
+
+    @GetMapping("/teste-api-erro")
+    private String testeErro() {
+          throw new RuntimeException("Ouve uma falha ao tentar executar a chamada, por favor tente novamente mais tarde.");
     }
 }
