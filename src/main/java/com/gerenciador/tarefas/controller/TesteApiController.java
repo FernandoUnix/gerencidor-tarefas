a1480aca6d230c6ca46497f353082627da010603
Fernando Amaro <01fernando.ti@gmail.com>
2023-08-28 15:56:55 -0300

    removed

--- src/main/java/com/gerenciador/tarefas/controller/TesteApiController.java
+++ src/main/java/com/gerenciador/tarefas/controller/TesteApiController.java
@@ -11,14 +11,4 @@ public class TesteApiController {
     private String teste() {
         return "Sucesso";
     }
-    
-    @GetMapping("/teste-api-bem-vindo")
-    private String testeBemVindo(@RequestParam(name = "nome") String nome) {
-        return "Olá, seja muito bem vindo " + nome;
-    }
-
-    @GetMapping("/teste-api-erro")
-    private String testeErro() {
-          throw new RuntimeException("Ouve uma falha ao tentar executar a chamada, por favor tente novamente mais tarde.");
-    }
 }
